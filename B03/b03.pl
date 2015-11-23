ab25186b83e7525e4029b972a882f2b49c4c1b01
Michael Birkhoff <Mike@Michaels-MBP-4.fritz.box>
2015-11-23 09:38:25 +0100

    added b03

--- B03/b03.pl
+++ B03/b03.pl
@@ -2,14 +2,31 @@
 
 add(0,0,0).
 
-% rekursive Klausel
+% rekursive Klauseln
+% wur zaehlen Y runter
 
 add(0,Y,R):-
-	Y = succ(B),
-	R = succ(R2),
+	Y = succ(B),	% Y ist Nachfolger von B
+	R = succ(R2),	% R ist der Nachfolger von R2
 	add(0,B,R2).
 
+% wir zaehlen X runter
+
 add(X,Y,R):-
-	X = succ(A),
+	X = succ(A),	% X ist Nachfolger von A
 	R = succ(R2),
 	add(A,Y,R2).
+	
+	
+	
+	
+% X ist groesser als Y
+
+% Anker klausel
+
+greater_than(succ(_),0).
+
+% rekursive Klausel
+
+greater_than(succ(X),succ(Y)):-
+	greater_than(X,Y).
